10cabdf090d8803e1473a1df55de54a432e8262c
bethtechow <bethtechow@yahoo.com>
2017-11-20 01:40:50 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express')
 var app = express()
 
-// Load Controllers
-
+/// Load Controllers
+const SiteController = require("./controllers/SiteController.js")
+const PhotosController = require("./controllers/PhotosController.js")
 
 // Routes
+app.get("/", SiteController.Index)
+app.get("/about", SiteController.About)
+app.get("/contact", SiteController.Contact)
+app.get("/photos", PhotosController.Index)
+app.get("/photos/new", PhotosController.New)
+
+
+app.get("/", PhotoController.Index)
+app.get("/Show", PhotoController.Show)
+app.get("/New", PhotoController.New)
+app.get("/View", PhotoController.View)
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
